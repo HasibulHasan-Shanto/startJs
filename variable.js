32211893cb87425770aa9819a9cd29062df0d757
shanto <shantohasibulhasan379@gmail.com>
2026-03-07 15:33:02 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,16 @@
+// console.log('today im start learning js')
+
+// variable declear korar jonno 5 ta jinis need 
+//1: var keyword
+// 2: variable name
+//3: = 
+//4: variable er man
+//5: ;
+
+
+var name = 'hasibul hasan shanto';
+// console.log(name)
+ var weight = 37;
+ console.log(weight)
+ var price;
+ console.log(price)

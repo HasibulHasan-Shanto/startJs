4dc57b00a40cd29ad3c9a8994bb524ad22f1866f
shanto <shantohasibulhasan379@gmail.com>
2026-03-09 13:48:00 +0600

    addded type and naming

---
+++ naming.js
@@ -0,0 +1,28 @@
+//console.log('today im learning naming variable')
+// variable er name lekhar jonno amader jei jinish gulo kheyal rakha dorker 
+
+// keyword diye kokhono variable er nam start kora jabe na like--
+
+var true = 'shanto'
+var var = 40
+var false = 'sadia'
+
+// variable er first letter kokhono number diye declear kora jabe na but tar pore number use kora jabe like ---
+
+var 3money = 4500
+var money2 = 3400
+
+// quotation diye kokhono variable declear kora jabe na like---
+
+// var 'name' = 'hasibul'
+
+// variable er name er moddhe kokhono space use kora jabe na like --
+ 
+var full name = 'hasaibul hasan shanto'
+
+// long diye variable declear korar time ai 3 ti maddhom use kora jay like---
+
+var my_current_address = '18/4 middle begunbari tejgoan dhaka' // ai type er variable k---snake case bole
+var myCurrentAddress = '18/4 middle begunbari tejgoan dhaka'// ai type er variable ke ---camel case bole 
+
+var MyCurrentAddress = '18/4 middle begunbari tejgoan dhaka'// ai type er variable ke ---pascal case bole
---
+++ type.js
@@ -0,0 +1,28 @@
+//console.log('type')
+
+var name = 'hasibul hasan shanto'
+// console.log(name)
+// string er moddhe kono kisu likhee variable declear korle seta ke string type variable bol 
+
+
+// true and false diye jodi kono variable declear kora hoy seta ke bulean type variable bole like
+
+var isRich = true
+console.log(isRich)
+var passed = false
+//console.log(passed)
+
+// variable ki type er seta check korar jonno typeof keyword use kora hoy like
+
+var bookPrice = 340
+console.log(typeof bookPrice)
+var result = 97.5
+console.log(typeof result)
+var fruiteName = 'apple'
+console.log(typeof fruiteName)
+console.log(typeof isRich)
+console.log(typeof passed)
+
+const country = 'Bangladesh'
+console.log(country)
+console.log(typeof country)

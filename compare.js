3e118a4463e3b1029e0cf08b3d2dcc16eb00ce44
shanto <shantohasibulhasan379@gmail.com>
2026-03-10 19:37:03 +0600

    added compare and ifElse

---
+++ compare.js
@@ -0,0 +1,21 @@
+console.log('today im learning simple compare')
+
+var orangePrice = 340
+var applePrice = 200
+
+var result = orangePrice > applePrice
+// console.log(result)
+var result = orangePrice < applePrice
+// console.log(result)
+var result = orangePrice == applePrice
+// console.log(result)
+
+var price = 100
+var price2 = '100'
+
+var final = price == price2
+console.log(final)
+var final = price === price2
+console.log(final)
+// javaScrip e == sudhu variable er man check kore and === variable er man and typeof 2 tai check kore 
+
---
+++ ifElse.js
@@ -0,0 +1,38 @@
+console.log('today im learning simple condition if else')
+
+var number = -12
+
+if(number > 0){
+    console.log('this is positive number')
+}else if(number < 0 ){
+    console.log('this is negative number')
+}
+else{
+    console.log('this is zero')
+}
+
+var age = 0
+
+if(age >= 18){
+    console.log('you can vote')
+}
+else{
+    console.log('you cannot vote')
+}
+
+var mark = 40
+
+if(mark >= 40){
+    console.log('you are pass')
+}
+else{
+    console.log('you are fail')
+}
+
+var temperature = 40
+
+if(temperature >= 40){
+    console.log('today is hot day')
+}else{
+    console.log('today is normal day')
+}

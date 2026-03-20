fb895d97acdbbca5a852cc9605fd3e3c3a1442a2
shanto <shantohasibulhasan379@gmail.com>
2026-03-20 09:07:50 +0600

    added dom

---
+++ Dom/dom.js
@@ -0,0 +1,9 @@
+// javaScript e specific sob tag ke select kore kaj korar jonno document.getElementByTagName use kora hoy like--->
+
+const paragraphs = document.getElementsByTagName('p')
+// console.log(paragraph)
+
+// jodi paragraph er sudhu lekha take dekhte chai taholee .innerText use korte hobe like --->
+for (const paragraph of paragraphs) {
+     paragraph.style.color = 'lightblue'
+}

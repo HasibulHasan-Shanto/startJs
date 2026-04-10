1abfbcefc0a8de70dceadb253ea23cbfa8b78f04
shanto <shantohasibulhasan379@gmail.com>
2026-04-10 23:28:17 +0600

    added dataType

---
+++ Es6/dataType.js
@@ -0,0 +1,9 @@
+console.log('today im learning data type')
+
+// primitive date type
+// Number, string, bulean, undefine, null ai gulo hocche primitive date type 
+
+//Non primitive datea type
+
+// Object, array ai gulo hocche non primitive data type
+

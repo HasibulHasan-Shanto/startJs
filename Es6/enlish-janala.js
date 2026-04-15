524f33ee876dc5c03b2bd7a4a1c4858013234de7
shanto <shantohasibulhasan379@gmail.com>
2026-04-15 23:44:32 +0600

    added english janala

--- Es6/enlish-janala.js
+++ Es6/enlish-janala.js
@@ -6,21 +6,75 @@ const allLesson = async () => {
     displayLesson(data.data)
 }
 
+
+const singleLesson = (id) => {
+    fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+        .then(res => res.json())
+        .then(data => displaySingleLesson(data.data))
+
+}
+const displaySingleLesson = (words) => {
+    console.log(words)
+    const lessonContainer = document.getElementById('lesson-container')
+    lessonContainer.innerHTML = ''
+    if (words.length === 0) {
+        lessonContainer.innerHTML = `
+        <div class="col-span-3 flex justify-center items-center min-h-[300px]">
+            <div class="bg-white shadow-lg rounded-xl p-10 text-center border w-[700px]">
+                <h2 class="text-2xl font-bold text-gray-700 mb-2">
+                    😢 No Data Found
+                </h2>
+                <p class="text-gray-500">
+                    This lesson has no words available.
+                </p>
+            </div>
+        </div>
+    `
+        return
+    }
+    for (const word of words) {
+        const div = document.createElement('div')
+        div.className = "bg-[#FFFFFF] py-20 px-5 text-center rounded-xl shadow-md"
+        div.innerHTML = `
+                                <h1 class="font-bold text-[24px]">
+                            ${word.word}
+                        </h1>
+                        <p class="font-semibold text-[18px]">
+                            Meaning/Pronunciation
+                        </p>
+                        <h2 class="font-bold text-[18px]">
+                            ${word.meaning} / ${word.pronunciation}
+                        </h2>
+                        <div class="flex items-center justify-between">
+                        <div class="bg-gray-300 px-3 py-2 text-center rounded-sm">
+                        <i class="fa-solid fa-circle-info"></i>
+                        </div>
+                        <div class="bg-gray-300 px-3 py-2 text-center rounded-sm">
+                        <i class="fa-solid fa-volume-high"></i>
+                        </div>
+                        
+                        
+                        </div>
+        `
+        lessonContainer.appendChild(div)
+    }
+
+}
 const displayLesson = (lessons) => {
     // console.log(lessons)
     const lessonBtn = document.getElementById('lesson-btn')
     lessonBtn.innerHTML = ''
     for (const lesson of lessons) {
-        console.log(lesson)
+        // console.log(lesson)
         const btn = document.createElement('button')
-        btn.className = "active border border-[#422AD5] rounded-sm py-2 px-4 font-bold text-[14px] text-[#422AD5]"
+        btn.className = "lesson-btn border border-[#422AD5] rounded-sm py-2 px-4 font-bold text-[14px] text-[#422AD5]"
         btn.innerHTML = `
                     <i class="fa-solid fa-book-open"></i>
                     Lesson - ${lesson.level_no}
-                </button>
         `
         btn.addEventListener('click', () => {
-            const allButton = document.querySelectorAll('.active')
+            singleLesson(lesson.level_no)
+            const allButton = document.querySelectorAll('.lesson-btn')
             for (const singleButton of allButton) {
                 singleButton.classList.remove('bg-[#422AD5]', 'text-white')
                 singleButton.classList.add('text-[#422AD5]')
@@ -30,4 +84,5 @@ const displayLesson = (lessons) => {
         lessonBtn.appendChild(btn)
     }
 }
+
 allLesson()
